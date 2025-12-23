23d8bc12f7a06817327d8be5b646307788bd2b86
AshPrajapati <ashprajapati67@gmail.com>
2025-12-23 15:48:58 +0530

    add test and return true if year is divisible by 400

--- leap_year_kata/test/leap_year_test.py
+++ leap_year_kata/test/leap_year_test.py
@@ -12,3 +12,7 @@ def test_5_is_leap_year():
 def test_year_is_divisible_by_4_is_leap_year():
     assert is_leap_year(2008) == True
     assert is_leap_year(2012) == True
+
+
+def test_year_is_divisible_by_400_is_leap_year():
+    assert is_leap_year(400) == True
