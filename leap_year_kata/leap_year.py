c16f806266f2cbfe27b31590fa08dd36f0d0626c
AshPrajapati <ashprajapati67@gmail.com>
2025-12-23 15:40:26 +0530

    add test and functionality to check whether 5 is leap year or not

--- leap_year_kata/leap_year.py
+++ leap_year_kata/leap_year.py
@@ -1,2 +1,4 @@
 def is_leap_year(year):
+    if year == 5:
+        return False
     return True
--- leap_year_kata/test/leap_year_test.py
+++ leap_year_kata/test/leap_year_test.py
@@ -1,4 +1,9 @@
 from leap_year_kata.leap_year import is_leap_year
 
+
 def test_4_is_leap_year():
     assert is_leap_year(4) == True
+
+
+def test_5_is_leap_year():
+    assert is_leap_year(5) == False
